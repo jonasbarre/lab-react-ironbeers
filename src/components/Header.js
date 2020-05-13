b2a3ac91006485bcf7d550470305d6cf66e57bbf
Jonas <jonas@smartvizual.com>
2020-05-13 16:40:12 +0200

    iterations 4 & 5 done

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
       <Link to='/' >
       <img
         src="https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png"
-        height="100px"
+        width="100%"
         alt="Back to homepage"
       />
       </Link>
--- src/components/ShowBeer.js
+++ src/components/ShowBeer.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Header from './Header';
 
-// This has to be a Class Component
+export default class ShowBeer extends Component {
+  state = {
+    beer: {}
+  }
 
-export default function ShowBeer(props) {
-
-    state = {
-        beers:[]
-      }
-
-    componentDidMount() {
-        console.log('Beers.js - component did mount');
-        fetch('https://ih-beers-api2.herokuapp.com/beers')
+  componentDidMount() {
+    console.log('ShowBeer.js - component did mount');
+    fetch(`https://ih-beers-api2.herokuapp.com/beers/${this.props.match.params.id}`)
       .then(res => res.json())
       .then(result => {
         console.log(result)
-        this.setState({beers: result})
+        this.setState({ beer: result })
       })
       .catch(e => console.log(e))
-      }
-
+    }
+  render() {
     return (
-        <div>
-            {props.match.params.id}
-        </div>
+      <React.Fragment>
+      <Header />
+      <div>
+                    <h1>{this.state.beer.name}</h1>
+                    <img src={this.state.beer.image_url} alt={this.state.beer.name} className='beerimg' />
+                    <h2>{this.state.beer.tagline}</h2>
+                    <h3>First brewed {this.state.beer.first_brewed}</h3>
+                    <h4>Attenuation level {this.state.beer.attenuation_level}</h4>
+                    <p>{this.state.beer.description}</p>
+                    <p>{this.state.beer.contributed_by}</p>
+      </div>
+      </ React.Fragment>
     )
+  }
 }
